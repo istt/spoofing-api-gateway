134342d04c18a01556bc0d77c0fe9f28189c8695
Trung Nguyen Dinh <nguyendinhtrung141@gmail.com>
2021-07-19 09:58:52 +0700

    Initial version of apiGateway generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/istt/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.istt.service.dto;
---
+++ src/test/java/com/istt/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.istt;
+
+import com.istt.ApiGatewayApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = ApiGatewayApp.class)
+public @interface IntegrationTest {
+}
